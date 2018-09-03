f0755e792aa2210161b9c19ca463d1bb5005a290
Yuji Hanamura <yuji.developer@gmail.com>
2018-09-03 22:25:43 +0900

    Re-implement using `match` expression

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
 fn main() {
     for n in 1..101 {
-        if n % 15 == 0 {
-            println!("FizzBuzz");
-        } else if n % 5 == 0 {
-            println!("Buzz");
-        } else if n % 3 == 0 {
-            println!("Fizz");
-        } else {
-            println!("{}", n);
+        match n {
+            _ if n % 15 == 0 => println!("FizzBuzz"),
+            _ if n % 5 == 0 => println!("Buzz"),
+            _ if n % 3 == 0 => println!("Fizz"),
+            _ => println!("{}", n),
         }
     }
 }
